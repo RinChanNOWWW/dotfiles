61702b4adfd0b489cd0e3eb671ecfc8364a7d698
RinChanNOWWW <hzy427@gmail.com>
2022-02-05 09:38:25 +0800

    update leetcode.mk

--- linux/leetcode.mk
+++ linux/leetcode.mk
@@ -1,7 +1,14 @@
 DATE = $(shell date +"%Y-%m-%d")
 WEEKLY-CONTEST-DIR = 周赛/第$(CONTEST_NO)场\($(DATE)\)
 
+define HEADER
+#include <bits/stdc++.h>
+using namespace std;
+endef
+export HEADER
+
 CC = clang++
+CPPSTD = -std=c++17
 CFLAGS = -g -O
 RM = rm -f
 
@@ -11,10 +18,10 @@ TARGETS = $(SOURCES:.cpp=.out)
 all: $(TARGETS)
 
 $(TARGETS): %.out : %.cpp
-	$(CC) $(CFLAGS) -o $@ $<
+	$(CC) $(CFLAGS) $(CPPSTD) -o $@ $<
 
 clean:
-	@rm -f $(TARGETS)
+	@${RM} -f $(TARGETS)
 	@find . -name '*.out' -type f -exec $(RM) {} \;
 
 create-weekly-contest:
@@ -27,8 +34,8 @@ else ifneq ($(shell echo $(CONTEST_NO) | sed 's/[0-9]//g'),)
 else
 	@echo "创建周赛目录: $(WEEKLY-CONTEST-DIR)"
 	@mkdir -p $(WEEKLY-CONTEST-DIR);
-	@touch $(WEEKLY-CONTEST-DIR)/1.cpp
-	@touch $(WEEKLY-CONTEST-DIR)/2.cpp
-	@touch $(WEEKLY-CONTEST-DIR)/3.cpp
-	@touch $(WEEKLY-CONTEST-DIR)/4.cpp
+	@echo "$$HEADER" > $(WEEKLY-CONTEST-DIR)/1.cpp
+	@echo "$$HEADER" > $(WEEKLY-CONTEST-DIR)/2.cpp
+	@echo "$$HEADER" > $(WEEKLY-CONTEST-DIR)/3.cpp
+	@echo "$$HEADER" > $(WEEKLY-CONTEST-DIR)/4.cpp
 endif	
